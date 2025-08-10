cbcd1498082fe00456d2ab31f16628dc3e197fe1
zippostorm <toghkklli@gmail.com>
2025-08-10 10:52:18 +0300

    Register form create

--- nextjs-client/src/features/auth/components/RegisterForm.tsx
+++ nextjs-client/src/features/auth/components/RegisterForm.tsx
@@ -4,6 +4,16 @@ import { useForm } from "react-hook-form";
 import { AuthWrapper } from "./AuthWrapper";
 import { RegisterSchema, TypeRegisterSchema } from "../schemas/register.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shared/components/ui/form";
+import { Input } from "@/shared/components/ui/input";
+import { Button } from "@/shared/components/ui/button";
 
 export function RegisterForm() {
   const form = useForm<TypeRegisterSchema>({
@@ -28,7 +38,76 @@ export function RegisterForm() {
       backButtonHref="/auth/login"
       isShowSocial
     >
-      RegisterForm
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="grid gap-4 space-y-2"
+        >
+          <FormField
+            control={form.control}
+            name="displayName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Display name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Nikita" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="example@gmail.com"
+                    type="email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="******" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="passwordRepeat"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password repeat</FormLabel>
+                <FormControl>
+                  <Input placeholder="******" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full">
+            Register
+          </Button>
+        </form>
+      </Form>
     </AuthWrapper>
   );
 }
